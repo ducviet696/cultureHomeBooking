2d9439d7cabd573f791c8caeac67369b73b493d3
Viet Anh <anhndv1408@gmail.com>
2019-03-05 23:55:20 +0700

    Update UI

--- app/src/main/java/com/swp/culturehomestay/Activity/MainActivity.java
+++ app/src/main/java/com/swp/culturehomestay/Activity/MainActivity.java
@@ -1,35 +1,49 @@
-package com.swp.culturehomestay.Activity;
+package com.swp.culturehomestay.activity;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.swp.culturehomestay.R;
+import com.swp.culturehomestay.fragments.main.FavoriteFragment;
+import com.swp.culturehomestay.fragments.main.HomeFragment;
+import com.swp.culturehomestay.fragments.main.MoreFragment;
+import com.swp.culturehomestay.fragments.main.NotificationFragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
+    private FragmentManager fm;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentTransaction fragmentTransaction = fm.beginTransaction();
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    fragmentTransaction.replace(R.id.fragment,new HomeFragment());
+                    fragmentTransaction.commit();
                     return true;
                 case R.id.navigation_favorite:
-                    mTextMessage.setText(R.string.title_favorite);
+                    fragmentTransaction.replace(R.id.fragment,new FavoriteFragment());
+                    fragmentTransaction.commit();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    fragmentTransaction.replace(R.id.fragment,new NotificationFragment());
+                    fragmentTransaction.commit();
                     return true;
                 case R.id.navigation_more:
-                    mTextMessage.setText(R.string.title_more);
+                    fragmentTransaction.replace(R.id.fragment,new MoreFragment());
+                    fragmentTransaction.commit();
                     return true;
             }
             return false;
@@ -40,10 +54,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        fm = getSupportFragmentManager();
     }
 
 }
