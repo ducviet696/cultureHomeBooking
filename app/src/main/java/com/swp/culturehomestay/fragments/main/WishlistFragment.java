6677716b88733b67627b69c5497e67b5aefec8a5
VietCD <lienminhhyenthoai12396@gmail.com>
2019-03-10 18:17:58 +0700

    commit layout wishlist

--- app/src/main/java/com/swp/culturehomestay/fragments/main/WishlistFragment.java
+++ app/src/main/java/com/swp/culturehomestay/fragments/main/WishlistFragment.java
@@ -3,11 +3,17 @@ package com.swp.culturehomestay.fragments.main;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.swp.culturehomestay.R;
+import com.swp.culturehomestay.adapter.VerticalListHomeAdapter;
+import com.swp.culturehomestay.entity.HomeStay;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +30,19 @@ public class WishlistFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wishlist, container, false);
+        View view = inflater.inflate(R.layout.fragment_wishlist, container, false);
+        //view.findViewById(R.id.rvWish);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.rvWish);;
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager( getContext(), LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(layoutManager);
+        ArrayList<HomeStay> homeStays = new ArrayList<>();
+        homeStays.add(new HomeStay(R.drawable.homestay, "Villa", 4, "HomeStay one", 500, "Hanoi"));
+        homeStays.add(new HomeStay(R.drawable.st2, "Villa", 7, "HomeStay two", 1000, "Thach That, Hoa Lac"));
+        homeStays.add(new HomeStay(R.drawable.homestay, "Villa", 5, "Vina Taba Vil", 200, "FPT University"));
+        VerticalListHomeAdapter adapter = new VerticalListHomeAdapter( homeStays, getContext());
+        recyclerView.setAdapter(adapter);
+        return view;
     }
 
 }
