936a2e2fb86bb3541a1a9d1f84f1390c109aa69e
NhanVT <nhanadam95@gmail.com>
2019-03-14 03:54:41 +0700

    create dialog of amout
    create dialog of amout

--- app/src/main/java/com/swp/culturehomestay/fragments/main/HomeFragment.java
+++ app/src/main/java/com/swp/culturehomestay/fragments/main/HomeFragment.java
@@ -1,17 +1,21 @@
 package com.swp.culturehomestay.fragments.main;
 
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.swp.culturehomestay.R;
+import com.swp.culturehomestay.activity.MainActivity;
 
 import java.util.Calendar;
 
@@ -25,7 +29,7 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    TextView txtCheckInDate, txtCheckOutDate, iconSearch;
+    TextView txtCheckInDate, txtCheckOutDate, iconSearch, textAmount, iconAmount;
     EditText txtSearch;
 
     Calendar c;
@@ -40,15 +44,19 @@ public class HomeFragment extends Fragment {
         txtCheckOutDate = (TextView) view.findViewById(R.id.txtCheckOutDate);
         iconSearch = (TextView) view.findViewById(R.id.iconSearch);
         txtSearch = (EditText) view.findViewById(R.id.TextSearch);
+        iconAmount = (TextView) view.findViewById(R.id.iconAmount);
+        textAmount = (TextView) view.findViewById(R.id.textAmount);
 
-        iconSearch.setOnClickListener(SearchClick);
-        txtSearch.setOnClickListener(SearchClick);
+        iconAmount.setOnClickListener(amountClick);
+        textAmount.setOnClickListener(amountClick);
+        iconSearch.setOnClickListener(searchClick);
+        txtSearch.setOnClickListener(searchClick);
         txtCheckInDate.setOnClickListener(txtCheckInDateOnClick);
         txtCheckOutDate.setOnClickListener(txtCheckOutDateOnClick);
         return view;
     }
 
-    View.OnClickListener SearchClick = new View.OnClickListener() {
+    View.OnClickListener searchClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
 
@@ -92,6 +100,59 @@ public class HomeFragment extends Fragment {
             }, day, mounth, year);
             dpd.getDatePicker().setMinDate(System.currentTimeMillis());
             dpd.show();
+        }
+    };
+
+    View.OnClickListener amountClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
+            View mView = getLayoutInflater().inflate(R.layout.home_search_amount, null);
+            final TextView txtAdd, txtRemove, txtAmountDetail;
+            Button btnSave;
+
+            txtAdd = (TextView) mView.findViewById(R.id.add);
+            txtRemove = (TextView) mView.findViewById(R.id.remove);
+            txtAmountDetail = (TextView) mView.findViewById(R.id.amount);
+            btnSave = (Button) mView.findViewById(R.id.bntSave);
+
+            // Acction of add click
+            txtAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String strAmout = txtAmountDetail.getText().toString();
+                    txtAmountDetail.setText(Integer.parseInt(strAmout) + 1 + "");
+                }
+            });
+
+            // Acction of remove click
+            txtRemove.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String strAmout = txtAmountDetail.getText().toString();
+                    if (Integer.parseInt(strAmout) > 0) {
+                        txtAmountDetail.setText(Integer.parseInt(strAmout) - 1 + "");
+                    } else {
+
+                    }
+                }
+            });
+
+            // Acction of save click
+//            btnSave.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    View View = getLayoutInflater().inflate(R.layout.fragment_home, null);
+//                    TextView txtAmount;
+//                    txtAmount = (TextView) View.findViewById(R.id.amount);
+//                    Toast.makeText(MainActivity.this, ""+txtAmount.toString(), Toast.LENGTH_LONG).show();
+////                    txtAmount.setText(txtAmountDetail.getText().toString());
+//                }
+//            });
+
+            mBuilder.setView(mView);
+            AlertDialog dialog = mBuilder.create();
+            dialog.show();
         }
     };
 }
